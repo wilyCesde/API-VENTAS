4a5c3c7354c3d18daf88b0b404c22ada49468583
willian <wruizza@cesde.net>
2023-01-10 12:27:17 -0500

    crea metodo regsitrar

--- SistemaVentas.DAL/Repositorios/VentaRepository.cs
+++ SistemaVentas.DAL/Repositorios/VentaRepository.cs
@@ -12,26 +12,75 @@ namespace SistemaVentas.DAL.Repositorios
 
 
         //generamos el contructor
-        public VentaRepository(DbventaContext dbcontext)
+        public VentaRepository(DbventaContext dbcontext) : base(dbcontext)
         {
             _dbcontext = dbcontext;
         }
 
         public async Task<Venta> Registrar(Venta modelo)
         {
-            try
+            Venta ventaGenerada = new Venta();
+
+            using (var transaction = _dbcontext.Database.BeginTransaction())
             {
-                // Agregamos el modelo de venta al contexto de la base de datos
-                _dbcontext.Add(modelo);
-                // Guardamos los cambios en la base de datos
-                await _dbcontext.SaveChangesAsync();
-                // Retornamos el modelo de venta registrado
-                return modelo;
-            }
-            catch (Exception ex)
-            {
-                // Aquí puedes manejar el error de alguna manera, por ejemplo lanzando una excepción personalizada
-                throw new Exception("Error al registrar la venta en la base de datos", ex);
+                try
+                {
+                    foreach (DetalleVenta dv in modelo.DetalleVenta)
+                    {
+                        Producto producto_encontrado = _dbcontext.Productos.Where
+                            (p => p.IdProducto == dv.IdProducto).First();
+
+                        producto_encontrado.Stock = producto_encontrado.Stock - dv.Cantidad;
+                        _dbcontext.Productos.Update(producto_encontrado);
+
+                    }
+
+                    await _dbcontext.SaveChangesAsync();
+
+                    NumeroDocumento correlativo = _dbcontext.NumeroDocumentos.First();
+                    correlativo.UltimoNumero = correlativo.UltimoNumero + 1;
+                    correlativo.FechaRegistro = DateTime.Now;
+
+                    _dbcontext.NumeroDocumentos.Update(correlativo);
+                    await _dbcontext.SaveChangesAsync();
+
+                    int CantidadDigitos = 4;
+                    string ceros = string.Concat(Enumerable.Repeat("0", CantidadDigitos));
+                    string numeroVenta = ceros + correlativo.UltimoNumero.ToString();
+
+                    numeroVenta = numeroVenta.Substring(numeroVenta.Length - CantidadDigitos, CantidadDigitos);
+
+                    //ACTUALIZAR NUEMRO DE DOCUMENTO
+
+                    modelo.NumeroDocumento = numeroVenta;
+
+                    //Acceder base de dtos 
+
+                    await _dbcontext.Venta.AddAsync(modelo);
+
+                    //guardar de anera asyncrona 
+
+                    await _dbcontext.SaveChangesAsync();
+
+                    //llamar vena generada pasmos el modelo
+
+                    ventaGenerada = modelo;
+                    transaction.Commit();
+                    return ventaGenerada;
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw new Exception("Error al registrar la venta: " + ex.Message);
+
+
+
+
+
+                }
             }
         }
 
